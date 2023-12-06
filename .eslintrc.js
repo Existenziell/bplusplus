0eab1a0588ea45b98a0bb8f3fa6cf8b0dcfc41b6
Christof <bauer.christof@gmail.com>
2023-12-06 11:42:51 +0100

    Add satoshi icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  // env: {
-  //   browser: true,
-  //   es2021: true,
-  //   node: true,
-  // },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'plugin:react/recommended',
     'plugin:import/recommended',
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
-import Image from 'next/image'
+import SatoshiIcon from './components/SatoshiIcon'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen bg-grid bg-cover bg-center'>
       <div className='w-full items-center justify-between flex flex-col text-zinc-300'>
         <div className='h-full items-center justify-center flex flex-col'>
-          <h1 className='text-6xl'>B++</h1>
-          <h2 className='text-2xl'>Bitcoin Education</h2>
-          <p className='text-xs'>coming soon...</p>
-          <div className='mt-8'>
-            <Image
-              src={'/btc1.gif'}
-              width={100}
-              height={100}
-              alt='Rotating BTC'
-            />
-          </div>
+          <h1 className='text-8xl'>B++</h1>
+          <h2 className='mb-6'>Bitcoin Education</h2>
+          <SatoshiIcon />
+          <p className='text-xs mt-6'>coming soon...</p>
         </div>
         <div className='text-xs text-center'>
           Made with &#x1f9e1; by Chris

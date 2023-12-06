662e51239a2da860555faa90920611078a6ff871
Christof <bauer.christof@gmail.com>
2023-12-06 12:31:52 +0100

    Copy npub to clipboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,40 @@
+'use client'
+import { useState } from 'react'
 import SatoshiIcon from './components/SatoshiIcon'
 
 export default function Home() {
+  const address =
+    'npub1v7vslj3ewmdlqpzh3ta3glut80xg4vendfyvkypulydsqfmgc6kq90w3he'
+
+  const [showNotification, setShowNotification] = useState(false)
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(address)
+    setShowNotification(true)
+    setTimeout(() => {
+      setShowNotification(false)
+    }, 3000)
+  }
   return (
     <main className='flex min-h-screen bg-grid bg-cover bg-center'>
-      <div className='w-full items-center justify-between flex flex-col text-zinc-300'>
+      {showNotification && (
+        <div className='fixed top-0 bg-emerald-400 w-full text-center py-4 transition-all opacity-100'>
+          npub copied to clipboard!
+        </div>
+      )}
+      <div className='w-full items-center justify-between flex flex-col text-zinc-300 overflow-hidden'>
         <div className='h-full items-center justify-center flex flex-col'>
           <h1 className='text-8xl'>B++</h1>
           <h2 className='mb-6'>Bitcoin Education</h2>
           <SatoshiIcon />
           <p className='text-xs mt-6'>coming soon...</p>
         </div>
-        <div className='text-xs text-center'>
-          Made with &#x1f9e1; by Chris
+        <div className='text-xs text-center pb-2 overflow-hidden'>
+          Made with &#x1f9e1; by{' '}
+          <a className='hover:cursor-pointer' onClick={handleClick}>
+            Chris
+          </a>
           <br />
-          npub1v7vslj3ewmdlqpzh3ta3glut80xg4vendfyvkypulydsqfmgc6kq90w3he
         </div>
       </div>
     </main>

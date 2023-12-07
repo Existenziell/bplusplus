94aa581276610b2e2bd9af46bd9835aebd8a99dd
Christof <bauer.christof@gmail.com>
2023-12-07 20:31:24 +0100

    Mobile adaptations

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
   const satsPerDollar = Math.round(100_000_000 / price)
 
   return (
-    <div className="text-xs absolute top-4 left-4 bg-opacity-50 shadow-md bg-zinc-800 text-zinc-300 px-6 py-2">
+    <div className="text-xs absolute top-1 left-1 md:top-4 md:left-4 bg-opacity-50 shadow-md bg-zinc-800 text-zinc-300 px-2 py-1 md:px-6 md:py-2">
       <p>SATs per $: {satsPerDollar}</p>
       <p>Price: {priceFormatted}</p>
     </div>

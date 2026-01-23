140bf3c1e4d95d2ec9c31018bc070adacee89af6
Christof <bauer.christof@gmail.com>
2026-01-23 20:44:26 +0100

    fix: og image v3

--- app/utils/metadata.ts
+++ app/utils/metadata.ts
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 // Centralised site and OG config (used by layout and generatePageMetadata)
 export const SITE_URL = 'https://bplusplus.info'
-export const DEFAULT_OG_IMAGE = '/og/og.png'
+export const DEFAULT_OG_IMAGE = '/og/og.png?v=3'
 export const OG_LOGO = '/logo/logo.png'
 
 export function generatePageMetadata({

35b9b95050d143b73e96b956b160eccf22ff6797
Christof <bauer.christof@gmail.com>
2026-01-27 19:17:06 +0100

    test: move folders

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,12 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: 'tests/e2e',
+  outputDir: 'tests/test-results',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'list' : 'html',
+  reporter: process.env.CI ? 'list' : [['html', { outputFolder: 'tests/playwright-report' }]],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
